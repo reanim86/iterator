1375afb3c77561f873c77aa09eaa5f3f95322651
Aleksei Kudinov <reanim86@yandex.ru>
2022-11-07 23:14:21 +0300

    Change function FlatIterator(lisi of lists as many as you want)

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ class FlatIterator:
 
     def __iter__(self):
         self.item = []
-        for value in self.list_of_list:
-            for var in value:
-                self.item.append(var)
+        self.queue = [self.list_of_list]
+        while self.queue:
+            elem = self.queue.pop(-1)
+            if isinstance(elem, list):
+                self.queue.extend(elem)
+            else:
+                self.item.append(elem)
+        self.item.reverse()
         return self
 
     def __next__(self):
@@ -33,6 +38,23 @@ def test_1():
 
     assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
 
+def test_3():
+
+    list_of_lists_2 = [
+        [['a'], ['b', 'c']],
+        ['d', 'e', [['f'], 'h'], False],
+        [1, 2, None, [[[[['!']]]]], []]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_2),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
 
 if __name__ == '__main__':
     test_1()
+    test_3()
